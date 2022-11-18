ec5acaafbc147dbfe0cd43ac03251974b0d21a1b
小林正門 <kbysaaao@gmail.com>
2022-11-18 11:57:12 +0900

    testpush

--- Assets/script/a.cs
+++ Assets/script/a.cs
@@ -18,7 +18,7 @@ public class a : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //
+        //test
     }
 
     // Update is called once per frame
