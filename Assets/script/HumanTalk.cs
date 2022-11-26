927b94d29362b716895d9613ca75ac89216e1171
小林正門 <kbysaaao@gmail.com>
2022-11-26 13:36:05 +0900

    スクリクタブルオブジェクトを使って人にヒントをしゃべらせる

--- Assets/script/HumanTalk.cs
+++ Assets/script/HumanTalk.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * humanにヒントをしゃべらせる
+ */
 public class HumanTalk : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField]//画面に表示する
     UnityEngine.UI.Text textbox;
-
+    
     private Player playerScript;
 
     private string character = "";
+
+    [SerializeField]//スクリクタブルオブジェクトデータ
+    private CurrectData[] currectData = new CurrectData[2];
+    //どのスクリクタブルオブジェクトデータを使うか
+    private int n;
 
     //ここでどのキャラクターに話しかけたか判定して
     //その判定にあうキャラのセリフをあてる
@@ -18,31 +26,39 @@ public class HumanTalk : MonoBehaviour
     void Start()
     {
         playerScript = GameObject.Find("Player").GetComponent<Player>();
+        //ランダムにデータを選ぶ
+        n = Random.Range(1, 3);
+        Debug.Log(n);
     }
 
     void Update()
     {
-        //
+        //humanの名前を取得
         character = playerScript.GetObjectName();
+        //humanに触れたか
         if (playerScript.GetHumanFlag == true)
         {
-            switch (character)
+            for (int i = 0; i < currectData.Length; i++)
             {
-                case "Human1":
-                    textbox.text = "右";
-                    break;
-                case "Human2":
-                    textbox.text = "左";
-                    break;
-                case "Human3":
-                    textbox.text = "右";
-                    break;
-                case "Human4":
-                    textbox.text = "右";
-                    break;
-                default:
-                    textbox.text = "";
-                    break;
+                //どのhumanに話を聞きに行ったか
+                switch (character)
+                {
+                    case "Human1":
+                        textbox.text = currectData[n].CurrectDirection[0];
+                        break;
+                    case "Human2":
+                        textbox.text = currectData[n].CurrectDirection[1];
+                        break;
+                    case "Human3":
+                        textbox.text = currectData[n].CurrectDirection[2];
+                        break;
+                    case "Human4":
+                        textbox.text = currectData[n].CurrectDirection[3];
+                        break;
+                    default:
+                        textbox.text = "";
+                        break;
+                }
             }
         }
     }
